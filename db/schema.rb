9b499ffbd33502b68ce3f3d6ba526040dcf3c131
Joris Dewaele <joris.dewaele@tutanota.com>
2019-08-19 23:16:03 +0200

    Seed completed, without images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_140300) do
+ActiveRecord::Schema.define(version: 2019_08_19_192636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_140300) do
     t.datetime "updated_at", null: false
     t.boolean "is_artist", default: false
     t.boolean "is_bar_manager", default: false
+    t.string "name"
+    t.string "last_name"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,90 +8,188 @@
 
 require 'open-uri'
 
-puts "Destroying existing users :"
+puts "Destroying existing users/events/bars"
 User.destroy_all
 
-puts "Creating users"
+# Users:
+
+puts "Creating bar managers"
 
 bar_managers = []
 
-bar_manager = User.create(
+bar_manager_gégé = User.create(
   email: 'gégé.gerard@bar.be',
-  password: 'bargégébruxelles'
+  password: 'bargégébxl'
 )
+bar_managers.push(bar_manager_gégé)
 
-bar_managers.push(bar_manager)
-
-9.times do |i|
+4.times do |i|
   bar_manager = User.create(
   email: "bar#{i + 2}@bar.be",
-  password: "bar#{i + 2}bruxelles"
+  password: "bar#{i + 2}bxl"
   )
   bar_managers.push(bar_manager)
 end
 
-puts "Creating profiles"
+puts "Creating artists"
 
-addresses_profiles = [
+artists = []
+
+artist_gaspard = User.create(
+  email: 'gaspard@artist.be',
+  password: 'artistgaspardbxl'
+)
+artists.push(artist_gaspard)
+
+4.times do |i|
+  artist = User.create(
+  email: "artist#{i + 2}@artist.be",
+  password: "artist#{i + 2}bxl"
+  )
+  artists.push(artist)
+end
+
+puts "Creating personas"
+
+personas = []
+
+persona_balta = User.create(
+  email: 'balta@persona.be',
+  password: 'personabaltabxl'
+)
+personas.push(persona_balta)
+
+4.times do |i|
+  persona = User.create(
+  email: "persona#{i + 2}@persona.be",
+  password: "persona#{i + 2}bxl"
+  )
+  personas.push(persona)
+end
+
+# Profiles:
+
+addresses = [
   'Avenue Brugmann 40, 1190 Forest',
   'Avenue De Fré, 206 1180 Uccle',
   'Boulevard Lambermont 364, 1030 Schaerbeek',
   'Place Sainte-Catherine 12, 1000 Bruxelles',
   'Boulevard Anspach 24, 1000 Bruxelles',
   'Rue du Vieux Marché aux Grains 20, 1000 Bruxelles',
-  'Place St. Gery 1, 1000 Bruxelles',
+  'Place Saint-Gery 1, 1000 Bruxelles',
   'Avenue du Parc 10, 1060 Saint-Gilles',
   'Boulevard de la Woluwe 22, 1150 Woluwe-Saint-Pierre',
-  'Avenue du Parc Royal 12, 1020 Bruxelles'
+  'Avenue du Parc Royal 12, 1020 Bruxelles',
+  'Rue Belliard 4, 1040 Etterbeek',
+  'Chaussée de Haecht 6, 1210 Saint-Josse-ten-Noode'
 ]
 
-profiles = []
+photo_gégé = 'https://res.cloudinary.com/dx8gouewf/image/upload/v1566220564/g%C3%A9g%C3%A9_zazxnf.png'
+photo_gaspard = 'https://res.cloudinary.com/dx8gouewf/image/upload/v1566220564/g%C3%A9g%C3%A9_zazxnf.png'
+photo_balta = 'https://res.cloudinary.com/dx8gouewf/image/upload/v1565775771/qtw7khpdhemxtaax2wi6.svg'
+photo_random = 'https://res.cloudinary.com/dx8gouewf/image/upload/v1565775771/qtw7khpdhemxtaax2wi6.svg'
 
-photo_url1 = 'https://res.cloudinary.com/dx8gouewf/image/upload/v1566220564/g%C3%A9g%C3%A9_zazxnf.png'
-photo_url2 = 'https://res.cloudinary.com/dx8gouewf/image/upload/v1565775771/qtw7khpdhemxtaax2wi6.svg'
+puts "Creating profiles bar managers"
 
-profile = Profile.new(
+profile_gégé = Profile.new(
   username: 'gégégerard',
-  address: 'Place St. Gery 1, 1000 Bruxelles',
-  photo: photo_url1
+  name: 'Gégé',
+  last_name: 'Gerard',
+  address: 'Parvis De Saint-Gilles 40, 1060 Saint-Gilles',
+  photo: photo_gégé,
+  is_bar_manager: true
 )
-profile.user = bar_managers[0]
-profile.save!
+profile_gégé.user = bar_managers[0]
+profile_gégé.save!
 
-profiles.push(profile)
-
-9.times do |i|
-  profile = Profile.new(
+4.times do |i|
+  profile_bar_manager = Profile.new(
     username: "bar#{i+2}",
-    address: addresses_profiles[i],
-    photo: photo_url2
+    name: "Profile",
+    last_name: "Bar Manager #{i+2}",
+    address: addresses[i],
+    photo: photo_random,
+    is_bar_manager: true
   )
-  profile.user = bar_managers[i + 1]
-  profile.save!
-  profiles.push(profile)
+  profile_bar_manager.user = bar_managers[i + 1]
+  profile_bar_manager.save!
+end
+
+puts "Creating profiles artists"
+
+profile_gaspard = Profile.new(
+  username: 'gaspard',
+  name: 'FrouFrou',
+  last_name: 'Gaspard',
+  address: 'Rue de Savoie 42, 1060 Saint-Gilles',
+  photo: photo_gaspard,
+  is_artist: true,
+  artist_description: "I want to meet people, get a real human exchange, get comments on my work. I love to engage neighbourhood life."
+)
+profile_gaspard.user = artists[0]
+profile_gaspard.save!
+
+4.times do |i|
+  profile_artist = Profile.new(
+    username: "artist#{i+2}",
+    name: "Profile",
+    last_name: "Artist #{i+2}",
+    address: addresses[i+4],
+    photo: photo_random,
+    is_artist: true,
+    artist_description: Faker::Lorem.paragraph(sentence_count: 7)
+  )
+  profile_artist.user = artists[i + 1]
+  profile_artist.save!
+end
+
+puts "Creating profiles personas"
+
+profile_balta = Profile.new(
+  username: 'balta',
+  name: 'Balta',
+  last_name: 'Balthazard',
+  address: 'Rue Jourdan 30, 1060 Saint-Gilles',
+  photo: photo_balta
+)
+profile_balta.user = personas[0]
+profile_balta.save!
+
+4.times do |i|
+  profile_persona = Profile.new(
+    username: "persona#{i+2}",
+    name: "Profile",
+    last_name: "Persona #{i+2}",
+    address: addresses[i+8],
+    photo: photo_random
+  )
+  profile_persona.user = personas[i + 1]
+  profile_persona.save!
 end
 
 puts "Creating bars"
 
-addresses_bars = [
-  'Rue Belliard 4, 1040 Etterbeek',
-  'Chaussée de Haecht 6, 1210 Saint-Josse-ten-Noode',
-  'Boulevard Louis Schmidt 8, 1040 Etterbeek',
-  'Avenue du Parc 10, 1060 Saint-Gilles',
-  'Boulevard de la Woluwe 22, 1150 Woluwe-Saint-Pierre',
-  'Avenue du Parc Royal 12, 1020 Bruxelles',
-  'Boulevard de la Plaine 14, 1050 Ixelles',
-  'Boulevard Prince de Liège 16, 1070 Anderlecht',
-  'Rue du Vieux Marché aux Grains 20, 1000 Bruxelles',
-  'Place St. Gery 1, 1000 Bruxelles'
+bar_names = [
+  'Moonlit Table Bar',
+  'Singing Lion Ale House',
+  'Chill Barman Parlor',
+  'Spirited Pint Hideout'
 ]
 
-10.times do |i|
-  bar = Bar.new(
-    name: Faker::Restaurant.name,
-    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
-    address: addresses_bars[i]
+bar_gégé = Bar.new(
+  name: "Chez Gégé",
+  description: "Cosy bar promoting neighbourhood life. Organizing concerts for local artists. Discover new upcoming talent every week!",
+  address: "16 Rue Berckmans, 1060 Saint-Gilles"
 )
-bar.user_id = bar_managers[i].id
+bar_gégé.user = bar_managers[0]
+bar_gégé.save!
+
+4.times do |i|
+  bar = Bar.new(
+    name: bar_names[i],
+    description: Faker::Lorem.paragraph(sentence_count: 7),
+    address: addresses[i+2]
+)
+bar.user_id = bar_managers[i+1].id
 bar.save!
 end
