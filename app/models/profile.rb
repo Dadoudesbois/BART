f31d6913aa8f49f6cd76eb5f22a6e3d2bad05942
Daphne Laboul <daphne.laboul@gmail.com>
2019-08-19 18:11:36 +0200

    map

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,9 +5,10 @@ class Profile < ApplicationRecord
   validates :photo, presence: true
   validates :address, presence: true
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   def self.artist
     Profile.where(is_artist: true)
   end
 end
-
-
