7391981fad0852d83307698c792929ea7f8ef6ae
Daphne Laboul <daphne.laboul@gmail.com>
2019-08-19 16:05:15 +0200

    profiles controller , and email removed from profile -since already in user

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,64 @@
+class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @artists = []
+    @profiles = Profile.all
+    @profiles.each do |profile|
+      if profile.is_artist == true
+        @artists << profile
+      end
+    end
+  end
+
+  def show
+    @address = profile.address
+    @username = profile.username
+    @photo = profile.photo
+    @artist_description = profile.artist_description
+  end
+
+  def new
+    @user = current_user
+    @profile = Profile.new
+  end
+
+  def create
+    @user = current_user
+    @profile = Profile.new(profile_params)
+    @profile.user = @user
+
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path(@profile)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @profile.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_profile
+    @profile = profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :address, :photo, :is_artist, :is_bar_manager, :artist_description)
+  end
+end
