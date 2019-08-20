a05c36086c8905b4f33d794682ddca7d3b787c66
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-20 14:38:00 +0200

    controller methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,45 @@
 class MessagesController < ApplicationController
+  before_action :set_chatbox, only: [:create, :edit, :update, :destroy]
+  before_action :set_message, only: [:edit, :update, :destroy]
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message[:date] = Time.now
+    @message.chatbox = @chatbox
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to chatbox_path(@chatbox)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @message.destroy
+
+    redirect_to chatbox_path(@chatbox)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def set_chatbox
+    @chatbox = Chatbox.find(params[:chatbox_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
 
   resources :chatboxes, only: [:index, :show] do
-    resources :messages, only: [:new, :create, :edit, :update, :delete]
+    resources :messages, only: [:new, :create, :edit, :update, :destroy]
   end
 
   resources :bars do
