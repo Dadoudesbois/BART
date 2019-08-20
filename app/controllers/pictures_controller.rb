fbf90b3e3b22f60205ef798c3743e2e8bb4a1445
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-20 10:47:39 +0200

    new, create, delete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,35 @@
 class PicturesController < ApplicationController
+  before_action :set_bar, only: [:create, :destroy]
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    @picture.bar = @bar
+
+    if @picture.save
+      redirect_to bar_path(@bar)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+
+    redirect_to bar_path(@bar)
+  end
+
+  private
+
+  def set_bar
+    @bar = Bar.find(params[:bar_id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:url)
+  end
 end
