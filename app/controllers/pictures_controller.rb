0ac92e15a4047e59a1a9f129f829e457b39f0574
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-20 11:21:15 +0200

    added authorization in the controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_bar, only: [:create, :destroy]
+  before_action :bar_authorization [:new, :create, :destroy]
 
   def new
     @picture = Picture.new
@@ -24,6 +25,10 @@ class PicturesController < ApplicationController
   end
 
   private
+
+  def bar_authorization
+    redirect_back(fallback_location: root_path, flash: 'You are not authorized to perform this action') if @bar.user != current_user
+  end
 
   def set_bar
     @bar = Bar.find(params[:bar_id])
