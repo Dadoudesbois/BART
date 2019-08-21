7a8bb1908ca32485c3df2e27bba134e96f40fee8
Joris Dewaele <joris.dewaele@tutanota.com>
2019-08-21 10:45:57 +0200

    Bar pictures delete

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,9 @@ Rails.application.routes.draw do
 
 
   resources :bars do
-    resources :pictures, only: [:new, :create, :destroy]
+    resources :pictures, only: [:new, :create]
   end
+  resources :pictures, only: [:destroy]
 
   resources :events, only: [:show, :index, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
