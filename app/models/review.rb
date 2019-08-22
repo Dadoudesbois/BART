eeb51df5b721e479f6337aff2c2a710ea64c6387
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-22 16:49:58 +0200

    generated model and added validation/associations

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :bar
+  belongs_to :user
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5], message: "Must be between 0 and 5" }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :bars, dependent: :destroy
   has_one :profile, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   has_many :sent_chatboxes, :class_name => 'Chatbox', :foreign_key => 'sender_id', dependent: :destroy
   has_many :received_chatboxes, :class_name => 'Chatbox', :foreign_key => 'recipient_id', dependent: :destroy
