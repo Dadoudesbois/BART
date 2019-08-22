47bfe101117064fdc7a802277be7cb4822ee8848
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-22 17:22:08 +0200

    controller methods and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,52 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:edit, :update, :delete]
+  before_action :set_event, only: [:new, :create, :edit, :update]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.event = @event
+    @review.bar = @event.bar
+    @review.user = @event.user
+
+    if @review.save
+      redirect_to dashboard_profile(current_user.profile)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update
+      redirect_to dashboard_profile(current_user.profile)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @review.destroy
+
+    redirect_to dashboard_profile(current_user.profile)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 end
