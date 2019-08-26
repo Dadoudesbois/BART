3e29c89b7754d96a9c6b37971c8517a158f1fcf0
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-26 10:04:42 +0200

    fix

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -46,7 +46,6 @@ class ProfilesController < ApplicationController
         image_url: helpers.asset_url('star.png')
       }]
 
-    # @reviews = Review.art_reviews(@profile)
     @reviews = @profile.user.artist_reviews
   end
 
