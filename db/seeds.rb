beb57399642ebe39b29d1e8f98f72ce0516103fc
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-26 16:21:40 +0200

    added category

--- db/seeds.rb
+++ db/seeds.rb
@@ -290,7 +290,7 @@ bars.each do |bar|
       name: Faker::Name.unique.last_name,
       start_date: random_start_date,
       end_date: random_end_date,
-      description: Faker::Quote.matz
+      description: Faker::Quote.matz,
       category: ["Concert", "Exhibition", "Performance"].sample
     )
     event.remote_photo_url = "https://source.unsplash.com/120#{i}x810/?performance"
