8b456a5af370205846bb0efa547e602997552fc8
Daphne Laboul <daphne.laboul@gmail.com>
2019-08-27 16:34:05 +0200

    chat config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "ws://www.barsand.art/cable"
+  config.action_cable.allowed_request_origins = [ "http://www.barsand.art", "http://www.barsand.art" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
