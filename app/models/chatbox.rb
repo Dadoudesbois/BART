4c18a4936163633b77afea2399050fe0656de196
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-27 11:53:55 +0200

    associations

--- app/models/chatbox.rb
+++ app/models/chatbox.rb
@@ -1,5 +1,6 @@
 class Chatbox < ApplicationRecord
-  belongs_to :sender, :class_name => 'User'
-  belongs_to :recipient, :class_name => 'User'
-  has_many :messages, dependent: :destroy
+  # belongs_to :sender, :class_name => 'User'
+  # belongs_to :recipient, :class_name => 'User'
+  # has_many :messages, dependent: :destroy
+  has_many :messages
 end
