128465d44f8d1c5a7557287e17c2da1bb47137f9
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-27 15:45:58 +0200

    fixed model

--- app/models/chatbox.rb
+++ app/models/chatbox.rb
@@ -1,5 +1,5 @@
 class Chatbox < ApplicationRecord
-  # belongs_to :sender, :class_name => 'User'
-  # belongs_to :recipient, :class_name => 'User'
+  belongs_to :sender, :class_name => 'User'
+  belongs_to :recipient, :class_name => 'User'
   has_many :messages, dependent: :destroy
 end
