136c08acdce68afff34b48b804ba56b3e2324092
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-27 15:19:03 +0200

    fixed bug

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,14 +3,14 @@ class Message < ApplicationRecord
   belongs_to :chatbox
 
   validates :content, presence: true, allow_blank: false
-  after_create :boradcast_message
+  after_create :broadcast_message
 
   def from?(some_user)
     user == some_user
   end
 
   def broadcast_message
-    ActionCable.server.broadcast("chatbox_#{@chatbox.id}", {
+    ActionCable.server.broadcast("chatbox_#{chatbox.id}", {
       message_partial: ApplicationController.renderer.render(partial: "messages/message", locals: { message: self, user_is_messages_author: false }),
       current_user_id: user.id
     })
