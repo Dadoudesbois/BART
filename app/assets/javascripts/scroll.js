a0c1e435fdcd8d8beddb677b04f8461c0b02e681
Benoit Cavez <benoit.cavez@gmail.com>
2019-08-27 14:33:38 +0200

    undefined msg

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -2,4 +2,8 @@ function scrollLastMessageIntoView() {
   const messages = document.querySelectorAll('.message');
   const lastMessage = messages[messages.length - 1];
   lastMessage.scrollIntoView();
+
+  if (lastMessage !== undefined) {
+    lastMessage.scrollIntoView();
+  }
 }
